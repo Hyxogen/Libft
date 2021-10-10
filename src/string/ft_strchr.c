586d89c9d6a8c399d4bf55c0b95b88ca170a5b06
Daan Meijer <dmeijer@f1r6s15.codam.nl>
2021-10-10 11:15:12 +0200

    Make ft_strchrset non static

--- src/string/ft_strchr.c
+++ src/string/ft_strchr.c
@@ -11,3 +11,20 @@ char
 	}
 	return (0);
 }
+
+char
+	*ft_strchrset(const char *str, const char *charset)
+{
+	char	*closest_pos;
+	char	*char_pos;
+
+	closest_pos = 0;
+	while (*charset)
+	{
+		char_pos = ft_strchr(str, *charset);
+		if (char_pos && (!closest_pos || char_pos < closest_pos))
+			closest_pos = char_pos;
+		charset++;
+	}
+	return (closest_pos);
+}
