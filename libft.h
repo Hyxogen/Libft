db810af91ccc0a8e11622b846783f62777bb2e2c
Daan Meijer <dmeijer@f0r6s12.codam.nl>
2021-10-17 09:58:41 +0200

    Norminize all files

--- libft.h
+++ libft.h
@@ -1,7 +1,7 @@
 #ifndef LIBFT_H
 # define LIBFT_H
-#include <wchar.h>
-#include <stdlib.h>
+# include <wchar.h>
+# include <stdlib.h>
 
 # define LIBFT_ALPHA_LOW 'A'
 # define LIBFT_ALPHA_HIGH 'z'
@@ -20,11 +20,11 @@ typedef int	t_bool;
 # define TRUE 1
 # define FALSE 0
 
-typedef struct	s_list
+typedef struct s_list
 {
-	void					*content;
+	void			*content;
 	struct s_list	*next;
-}								t_list;
+}	t_list;
 
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
@@ -66,12 +66,12 @@ void	ft_putendl_fd(char *str, int fd);
 void	ft_putnbr_fd(int n, int fd);
 
 t_list	*ft_lstnew(void *content);
-void		ft_lstadd_front(t_list **lst, t_list *new);
-int			ft_lstsize(t_list *lst);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
-void		ft_lstadd_back(t_list **lst, t_list *new);
-void		ft_lstdelone(t_list *lst, void (*del)(void *));
-void		ft_lstclear(t_list **lst, void (*del)(void *));
-void		ft_lstiter(t_list *lst, void (*func)(void *));
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*func)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*func)(void *), void (*del)(void *));
 #endif
