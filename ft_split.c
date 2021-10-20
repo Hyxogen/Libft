2e32e17e62f83a4807bfdad82b31c02f52914c9c
Daan Meijer <dmeijer@f1r4s18.codam.nl>
2021-10-20 16:07:20 +0200

    Fix leaks

--- ft_split.c
+++ ft_split.c
@@ -13,6 +13,20 @@ static size_t
 }
 
 static void
+	clear_array(char **array)
+{
+	char	**start;
+
+	start = array;
+	while (*array)
+	{
+		free(*array);
+		array++;
+	}
+	free (start);
+}
+
+static int
 	fill_array(char **out, char *dup, const char *delim)
 {
 	char	*token_start;
@@ -25,20 +39,21 @@ static void
 		if (!dup && *token_start)
 		{
 			*out = ft_strdup(token_start);
-			*(out + 1) = 0;
+			if (!*out)
+				return (0);
 			break ;
 		}
 		else if (!dup)
-		{
-			*out = 0;
 			break ;
-		}
 		else if (dup - token_start > 1)
 		{
 			*out = ft_strdup(token_start);
+			if (!*out)
+				return (0);
 			out++;
 		}
 	}
+	return (1);
 }
 
 /*
@@ -63,10 +78,15 @@ char
 	ret = malloc(sizeof(char *) * size);
 	if (!ret)
 		return (0);
+	ft_bzero(ret, sizeof(char *) * size);
 	dup = ft_strdup(str);
 	if (!dup)
 		return (0);
-	fill_array(ret, dup, delim);
+	if (!fill_array(ret, dup, delim))
+	{
+		clear_array(ret);
+		return (0);
+	}
 	free(dup);
 	return (ret);
 }
