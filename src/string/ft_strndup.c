86292fe8886856a5949842eebe51be839f263758
Daan Meijer <daansander02@outlook.com>
2021-12-10 09:34:03 +0100

    Fix strndup

--- src/string/ft_strndup.c
+++ src/string/ft_strndup.c
@@ -6,7 +6,7 @@
 /*   By: dmeijer <dmeijer@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/12/08 14:13:36 by dmeijer       #+#    #+#                 */
-/*   Updated: 2021/12/10 09:20:59 by dmeijer       ########   odam.nl         */
+/*   Updated: 2021/12/10 09:33:31 by dmeijer       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,21 @@
 char
 	*ft_strndup(const char *s1, size_t n)
 {
-	size_t	len;
-	char	*dup;
-
+	size_t		len;
+	char		*dup;
+	const char	*cpy;
+	
 	len = 0;
-	while (*s1 && n)
+	cpy = s1;
+	while (*cpy && n)
 	{
 		len++;
-		s1++;
+		cpy++;
 		n--;
 	}
 	dup = malloc(len + 1);
 	if (dup == NULL)
 		return (NULL);
-	printf("len:%zu s1:%.*s\n", len, (int)len, s1);
 	ft_memcpy(dup, s1, len);
 	dup[len] = '\0';
 	return (dup);
