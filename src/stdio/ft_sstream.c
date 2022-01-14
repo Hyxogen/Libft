e7331c53a2edc35059ea56c4b7cf8684af3f6af3
Hyxogen <daansander02@outlook.com>
2022-01-14 06:58:15 +0100

    Norminize sstream

--- src/stdio/ft_sstream.c
+++ src/stdio/ft_sstream.c
@@ -5,25 +5,30 @@
 #include <ft_string.h>
 #include <ft_stdlib.h>
 
-ft_bool _stream_grow(t_sstream *stream, size_t new_size)
+ft_bool
+	_stream_grow(t_sstream *stream, size_t new_size)
 {
-	stream->m_ptr_begin = ft_realloc(stream->m_ptr_begin, stream->m_buffersize, new_size);
+	stream->m_ptr_begin = ft_realloc(stream->m_ptr_begin,
+			stream->m_buffersize, new_size);
 	stream->m_buffersize = new_size;
 	return (stream->m_ptr_begin == 0);
 }
 
-ft_bool stream_flush(t_sstream *stream, int fd)
+ft_bool
+	stream_flush(t_sstream *stream, int fd)
 {
-	int ret;
+	int	ret;
 
 	ret = write(fd, stream->m_ptr_begin, stream->m_write_offset);
 	stream->m_write_offset = 0;
-	return (ret);
+	return (ret >= 0);
 }
 
-ft_bool stream_write(t_sstream *stream, const char *str, size_t len)
+ft_bool
+	stream_write(t_sstream *stream, const char *str, size_t len)
 {
-	if (len + stream->m_write_offset >= stream->m_buffersize) {
+	if (len + stream->m_write_offset >= stream->m_buffersize)
+	{
 		_stream_grow(stream, stream->m_buffersize + SSTREAM_BUFFER_SIZE);
 		return (stream_write(stream, str, len));
 	}
@@ -32,7 +37,8 @@ ft_bool stream_write(t_sstream *stream, const char *str, size_t len)
 	return (1);
 }
 
-ft_bool stream_init(t_sstream *stream, size_t buffersize)
+ft_bool
+	stream_init(t_sstream *stream, size_t buffersize)
 {
 	stream->m_ptr_begin = malloc(buffersize);
 	stream->m_write_offset = 0;
