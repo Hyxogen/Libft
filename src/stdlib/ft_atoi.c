2291762ee5f346e344eef8a20854623231db6f52
Daan Meijer <daansander02@outlook.com>
2022-01-17 15:44:22 +0100

    Add overflow check to checked_atoi

--- src/stdlib/ft_atoi.c
+++ src/stdlib/ft_atoi.c
@@ -6,12 +6,13 @@
 /*   By: dmeijer <dmeijer@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/12/08 09:50:59 by dmeijer       #+#    #+#                 */
-/*   Updated: 2022/01/17 08:28:51 by dmeijer       ########   odam.nl         */
+/*   Updated: 2022/01/17 15:41:32 by dmeijer       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ctype.h"
 #include "ft_stdlib.h"
+#include <limits.h>
 
 int
 	ft_atoi(const char *str)
@@ -39,6 +40,24 @@ int
 }
 
 t_bool
+	_ft_checked_atoi(const char *str, int *out)
+{
+	long	val;
+	int		char_count;
+
+	val = 0;
+	char_count = 0;
+	while (ft_isdigit(*str) && char_count <= 12)
+	{
+		val = val * 10 + (*str - '0');
+		str++;
+		char_count++;
+	}
+	*out = (int) val;
+	return (*str == '\0' && val <= ((long) INT_MAX + 1));
+}
+
+t_bool
 	ft_checked_atoi(const char *str, int *out)
 {
 	int	sign;
@@ -54,11 +73,8 @@ t_bool
 	}
 	else if (*str == '+')
 		str++;
-	while (ft_isdigit(*str))
-	{
-		*out = *out * 10 + (*str - '0');
-		str++;
-	}
+	if (!_ft_checked_atoi(str, out))
+		return (FALSE);
 	*out *= sign;
-	return (*str == '\0');
+	return ((sign == -1 && *out < 0) || (sign == 1 && *out > 1));
 }
